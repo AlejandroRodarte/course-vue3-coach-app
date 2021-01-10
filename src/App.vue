8f5a2c63a7b2b3a1cad16744a180ec2928da749f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-01-09 19:14:35 -0700

    234. Working on the Main Layout & Styling

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,31 @@
 <template>
+  <app-header></app-header>
   <router-view></router-view>
 </template>
+
+<script>
+import Header from './components/layout/Header.vue';
+
+export default {
+  components: {
+    'app-header': Header
+  }
+}
+</script>
+
+<style>
+
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <section>
+        Filter
+    </section>
+
+    <section>
+        List of coaches
+    </section>
+
+</template>
