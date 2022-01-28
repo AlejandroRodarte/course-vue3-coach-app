f11d5fbf3d41273435e0b033302dc155472a7e33
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-01-27 22:15:27 -0700

    249. Filtering Requests for the Active Coach

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,8 +1,15 @@
 import * as types from './types';
+import * as authTypes from '../auth/types';
 
 const getters = {
-  [types.GET_REQUESTS]: (state) => state.requests,
-  [types.HAS_REQUESTS]: (state) => state.requests && state.requests.length > 0
+  [types.GET_REQUESTS]: (state, getters, rootState, rootGetters) => {
+    const userId = rootGetters[authTypes.GET_USER_ID];
+    return state.requests.filter((request) => request.coachId === userId);
+  },
+  [types.HAS_REQUESTS]: (state, getters) => {
+    const requests = getters[types.GET_REQUESTS];
+    return requests && requests.length > 0;
+  }
 };
 
 export default getters;
